d97775d675e62256ac66d0157f13edbb63a29fc4
Carissa Hicks <38417097+carissa406@users.noreply.github.com>
2023-03-06 15:48:41 -0600

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './img/Cottage.png'
+
+image.onload = () => {
+	c.drawImage(image, -10, 0)
+}
